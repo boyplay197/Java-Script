35b0c34db4aefee8cacbb1822b17cfb8181fe9aa
boyplay197 <adriany6996@gmail.com>
2026-08-10 09:10:59 -0300

    Delete EstrategiaPublicacaoArtigo.java

--- EstrategiaPublicacaoArtigo.java
+++
@@ -1,6 +0,0 @@
-public class EstrategiaPublicacaoArtigo implements EstrategiaPublicacao {
-    @Override
-    public void publicar() {
-        System.out.println("Artigo publicado com sucesso!");
-    }
-}
