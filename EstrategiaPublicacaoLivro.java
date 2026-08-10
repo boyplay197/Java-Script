fbf7db304843cb899f81a0744fe0e45e097302db
boyplay197 <adriany6996@gmail.com>
2026-08-10 09:11:37 -0300

    Delete EstrategiaPublicacaoLivro.java

--- EstrategiaPublicacaoLivro.java
+++
@@ -1,6 +0,0 @@
-public class EstrategiaPublicacaoLivro implements EstrategiaPublicacao {
-    @Override
-    public void publicar() {
-        System.out.println("Livro publicado com sucesso!");
-    }
-}
